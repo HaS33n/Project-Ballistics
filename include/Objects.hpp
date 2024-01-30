e83f9d3b42b4b1f0403d8c59057030280c6c5325
HaS33n <mikolajek.r@wp.pl>
2024-01-30 17:30:53 +0100

    updated gitignore and added some abstraction

--- include/Objects.hpp
+++ include/Objects.hpp
@@ -2,6 +2,16 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 
+class Force{
+public:
+    Force();
+    Force(sf::Vector3f v, float val){vec = v; value = val;}
+    
+private:
+    sf::Vector3f vec;
+    float value;
+};
+
 class Projectile{
 public:
     Projectile();
